d02f0f741cf6065d0c3d50342762234602dcb80a
HaNJI <henhooky@gmail.com>
2021-05-19 20:20:11 +0500

    Добавлен бэк :/

--- scripts.js
+++ scripts.js
@@ -7,12 +7,17 @@ function accuracySize() {
     document.getElementById("accuracy").innerHTML = size;
 }
 
+
 //Эта функция реализует вычислиение квадратного корня из заданного числа с заданной точностью
 function calculate(number, accuracy) {
+	number = parseFloat(number.replace(",", "."));
     let abs = Math.abs(number);
     let sqrt = Math.sqrt(abs);
     //округляем для заданной точности
-    let result = sqrt.ToFixed(accuracy);
+    let d = 1.0;
+    for(let i = 0; i < accuracy; i++)
+    	d *= 10.0;
+    let result = Math.round(sqrt * d) / d;
 
     if(number < 0) {
         result += "i";
@@ -20,10 +25,12 @@ function calculate(number, accuracy) {
     return result;
 }
 
+
+
 function getResult() {
     let accuracy = document.getElementById("size").value;
     let number = document.getElementById("input").value;
-    let result = calculate(accuracy, number);
+    let result = calculate(number, accuracy);
     document.getElementById("sqrtResult").value = result;
 }
 
